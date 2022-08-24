4e709331e493db2e6cc9bde475eb451a06a76ab4
dejano <dejano.with.tie@gmail.com>
2022-08-24 19:51:22 +0200

    update config

--- src/main/java/dev/karambol/cloudconfig/CloudConfigApplication.java
+++ src/main/java/dev/karambol/cloudconfig/CloudConfigApplication.java
@@ -2,7 +2,9 @@ package dev.karambol.cloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class CloudConfigApplication {
 
